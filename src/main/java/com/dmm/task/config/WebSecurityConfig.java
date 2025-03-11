2213f38dcad55b547d49dfb734a0f2bf6422a1c6
MeiMinemura <189607259+meiramune@users.noreply.github.com>
2025-03-11 18:37:56 +0900

    課題提出

--- src/main/java/com/dmm/task/config/WebSecurityConfig.java
+++ src/main/java/com/dmm/task/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,6 +36,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.authorizeRequests().antMatchers("/loginForm").permitAll() // loginFormは、全ユーザからのアクセスを許可
 				.anyRequest().authenticated(); // loginForm以外は、認証を求める
+		
+	    @Override
+		public void configure(WebSecurity web) throws Exception {
+			// 画像、JavaScript、cssは認可の対象外とする
+			web.debug(false).ignoring().antMatchers("/images/**", "/js/**", "/css/**");
+		}
 
 		// ログイン設定
 		http.formLogin() // フォーム認証の有効化
@@ -48,6 +55,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// ログアウト設定
 		http.logout().logoutSuccessUrl("/loginForm") // ログアウト成功時に遷移するパス
 				.permitAll(); // 全ユーザに対して許可
+		
+		
 	}
 
 }
