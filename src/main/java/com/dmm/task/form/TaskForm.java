60c8b596ad1cf752b8fc4c6217cdb60036c4b556
MeiMinemura <189607259+meiramune@users.noreply.github.com>
2025-03-12 18:07:30 +0900

    課題提出

--- src/main/java/com/dmm/task/form/TaskForm.java
+++ src/main/java/com/dmm/task/form/TaskForm.java
@@ -1,19 +1,14 @@
 package com.dmm.task.form;
 
-import java.time.LocalDate;
-
-import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 import lombok.Data;
 
 @Data
 public class TaskForm {
-	// titleへのバリデーション設定を追加
-	@Size(min = 1, max = 200)
+
 	private String title;
-	// textへのバリデーション設定を追加
-	@Size(min = 1, max = 200)
 	private String text;
-	private LocalDate date;
+	private LocalDateTime date;
 	private String name;
 }
