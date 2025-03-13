d0140ff55b049255092ba27f243c66ca99c74766
MeiMinemura <189607259+meiramune@users.noreply.github.com>
2025-03-13 19:46:11 +0900

    最終課題

--- src/main/java/com/dmm/task/EditController.java
+++ src/main/java/com/dmm/task/EditController.java
@@ -1,12 +1,8 @@
 package com.dmm.task;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EditController {
 
-	@GetMapping("/main/edit/{id}(id=${task.id})")
-	public String edit() {
-		return "edit";
-	}
+
 }
